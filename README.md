1b31ede5c6fada0b76ad885fc93fc44bbec0e8c5
IsaelSousa <toxicrootbeta@gmail.com>
2021-11-10 21:51:41 -0300

    adicionado sistema de cadastro de paciente

---
+++ README.md
@@ -0,0 +1,12 @@
+# Projeto Integrado Multidisciplinar IV - Cadastro de Pacientes
+
+Projeto PIM relacionado ao desenvolvimento de software solicitado pela faculdade UNIP.
+O projeto:
+* Apresentar um sistema de monitoramento que será utilizado pelos hospitais para cadastrar pacientes que forem diagnosticados com covid-19.
+
+Requisitos:
+* Desenvolver e aplicar os conhecimentos adquiridos nas disciplinas de Linguagem e Técnicas de Programação e Engenharia de Software.
+* Uso da linguagem C para desenvolvimento do sistema(através da IDE Codeblocks http://www.codeblocks.org/).
+
+Sobre o projeto:
+- 
--- main.c
+++ main.c
@@ -33,10 +33,24 @@ void TelaLogin(){
 
     if (strcmp(login, loginLoad) == 0 && strcmp(senha, senhaLoad) == 0){
         logged = 1;
+
+        printf("\n\nVerificando Credenciais!\n\n");
+        printf("1...");
+        Sleep(500);
+        printf("2...");
+        Sleep(500); 
+        printf("3...");
+        Sleep(500); 
+        printf("4...");
+        Sleep(500);     
+        printf("5...");                     
+        Sleep(500);
         printf("\n\nLogin efetuado com Sucesso!\n\n");
+        Sleep(1500);        
+        system("@cls||clear");
     } else {
 
-        printf("\n\nDADOS INVALIDOS!\n\n"); 
+        printf("\n\nUsuario nao Existe!\n\n"); 
         printf("Retornando a tela de login.\n");
 
         printf("1...");
@@ -61,7 +75,7 @@ void MostrarMenu(){
     printf("********Seja Bem Vindo Usuario*******\n");
     printf("*************************************\n");
 
-    printf("1) - CADASTRO DE CLIENTES\n");
+    printf("1) - CADASTRO DE PACIENTES\n");
     printf("2) - CADASTRO DE CORRETORES\n");
     printf("3) - SAIR DO SISTEMA\n\n");    
     printf("DIGITE A OPCAO DESEJADA: ");
@@ -71,10 +85,56 @@ void MostrarMenu(){
 
 }
 
-void CadastroClientes(){
+void CadastroPacientes(){
 
-    printf("\n\nAdicione os dados do Cliente\n");
+    char nome[45];
+    char cpf[20];
+    char telefone[16];
+    char endereco[100];
+    char idade[3];
+    char email[32];
+    char diagnosticData[10];
 
+    printf("\n\nAdicione os dados do Paciente\n");
+
+    // Nome
+    printf("Nome: ");
+    scanf("%[^\n]", nome);
+    getchar();      
+    // CFP
+    printf("CPF: ");
+    scanf("%[^\n]", cpf);
+    getchar();
+    // Telefone
+    printf("Telefone: ");
+    scanf("%[^\n]", telefone);
+    getchar();
+    // Endereço
+    printf("Endereco: ");
+    scanf("%[^\n]", endereco);
+    getchar();
+    // Idade
+    printf("Idade: ");
+    scanf("%[^\n]", idade);    
+    getchar();
+    // Email      
+    printf("Email: ");
+    scanf("%[^\n]", email); 
+    getchar(); 
+    // Data do Diagnostico  
+    printf("Data do Diagnostico: ");
+    scanf("%[^\n]", diagnosticData); 
+    getchar();  
+
+    printf("\n\nPaciente Cadastrado!\n");      
+
+    Sleep(1000);
+    printf("Nome: %s \n", nome);
+    printf("CPF: %d \n", cpf);
+    printf("Telefone: %s \n", telefone);
+    printf("Idade: %d \n", idade);
+    printf("Email: %s \n", email);
+    printf("Data do Diagnostico: %s \n", diagnosticData);
 }
 
 void CadastroCorretores(){
@@ -95,9 +155,9 @@ int main(){
 
         {
 
-            case 1 : // Gravar Clientes
+            case 1 : // Gravar Pacientes
 
-                CadastroClientes();
+                CadastroPacientes();
 
             break;
 
